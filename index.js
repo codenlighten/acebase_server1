36ec212fb9a4143ff8b917741b0ca39ab5127288
greg <codenlighten1@gmail.com>
2024-01-04 12:05:28 -0500

    standard

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const settings = {
   host: "chaindb.online",
   port: 443,
   https: {
-    certPath: "/home/greg/greg_dev/acebase_server/ssl/privkey.pem",
-    keyPath: "/home/greg/greg_dev/acebase_server/ssl/fullchain.pem",
+    certPath: "/home/greg/greg_dev/acebase_server/ssl/fullchain.pem", // Path to the certificate file
+    keyPath: "/home/greg/greg_dev/acebase_server/ssl/privkey.pem", // Path to the private key file
   },
 };
 const server = new AceBaseServer(dbname, settings);
